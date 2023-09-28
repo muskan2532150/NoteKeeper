50dc973331b54e987db7569d8cabeeaae95ffc30
muskan2532150 <rg2532150@gmail.com>
2023-09-28 15:34:00 +0530

    Create a CreateNode component to add notes

---
+++ note_keeper/src/component/CreateNote.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+import { PlusIcon } from '@heroicons/react/24/outline'
+import { useNote } from './NoteContext'
+
+const CreateNote = () => {
+    const [expand, setExpand] = useState(false);
+    const { note, updateNote } = useNote();
+    const [newnote, setNewnote] = useState({
+        title: '',
+        content: ''
+    })
+
+    const InputEvent = (e) => {
+        setNewnote((prevnote) => {
+            const { name, value } = e.target;
+            return {
+                ...prevnote,
+                [name]: value
+            }
+        });
+    }
+
+    const addEvent = (e) => {
+        e.preventDefault();
+        if (newnote.title.trim() !== '') {
+            let updatenote = [...note, newnote]
+            updateNote(updatenote);
+            setNewnote({
+                title: '',
+                content: ''
+            })
+            setExpand(false)
+        }
+        else {
+            console.log('Please Add Title')
+        }
+    }
+
+    return (
+        <div className='' onDoubleClick={() => (setExpand(false))}>
+
+            < form className='relative w-4/12 my-4 mx-auto' >
+                <div className="border rounded border-gray-900/10 shadow-2xl p-4 pb-0 ">
+                    <div className="flex sm:max-w-md  ">
+                        {expand ? <input
+                            type="text"
+                            name="title"
+                            id="title"
+                            autoComplete="off"
+                            className="block flex-1 w-full border-0 text-2xl text-gray-900 font-bold placeholder:text-xl placeholder:font-bold bg-transparent py-1.5 pl-1 placeholder:text-gray-900 placeholder:p-2 sm:text-sm sm:leading-6 focus:outline-none"
+                            placeholder="Title"
+                            onChange={InputEvent}
+                            value={newnote.title}
+                        /> : null}
+                    </div>
+                    <div>
+                        <textarea
+                            id="content"
+                            name="content"
+                            rows=''
+                            className="block w-full border-0 text-gray-400 font-bold text-base p-2 placeholder:text-base placeholder:font-bold placeholder:text-gray-400  sm:text-sm sm:leading-6 focus:outline-none"
+                            placeholder='Write a Note...'
+                            onChange={InputEvent}
+                            value={newnote.content}
+                            onClick={() => (setExpand(true))}
+                        />
+                    </div>
+                    {expand ? <div className="absolute -bottom-6 right-4 ">
+                        <button
+                            type="submit"
+                            onClick={addEvent}
+                            className="rounded-full shadow-xl bg-amber-500 text-lg cursor-pointer text-white hover:text-amber-500 hover:bg-white"
+                        >
+                            <PlusIcon className="h-10 w-10" aria-hidden="true" />
+                        </button>
+                    </div> : null}
+
+                </div>
+            </form >
+        </div>
+    )
+}
+
+export default CreateNote
+
+
+
