814cf3778b1e160cc472c9940ef64f8bedf719a8
muskan2532150 <rg2532150@gmail.com>
2023-09-28 16:34:17 +0530

    Remove unwanted code

--- note_keeper/src/component/Footer.jsx
+++ note_keeper/src/component/Footer.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { useNote } from './NoteContext'
 
 const Footer = () => {
-  const { note } = useNote();
-  console.log(note)
   return (
           <footer className={`bg-gray-200 py-4 w-full absolute bottom-0`}>
             <div className="container mx-auto text-center">
