8292c475f2c2492c0258086c6229d9609eafcf83
muskan2532150 <rg2532150@gmail.com>
2023-09-28 16:34:44 +0530

    Remove unwanted code

--- note_keeper/src/App.jsx
+++ note_keeper/src/App.jsx
@@ -1,21 +1,15 @@
-import react, { useState, createContext } from 'react'
 import Navbar from './component/Navbar'
-import Footer from './component/Footer'
 import CreateNote from './component/CreateNote'
-import Note from './component/Note'
+import Notes from './component/Notes'
 import { NoteProvider } from './component/NoteContext'
 
 const App = () => {
-
-  const note = createContext();
-
   return (
     <>
       <Navbar />
       <NoteProvider>
         <CreateNote />
-        <Note />
-        <Footer />
+        <Notes />
       </NoteProvider>
     </>
   )
