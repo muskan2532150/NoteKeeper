3a6104c61308feaff300186d1d967755d3b9e38e
muskan2532150 <rg2532150@gmail.com>
2023-09-28 16:03:06 +0530

    Create Notes component where Pagination and note are render

---
+++ note_keeper/src/component/Notes.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import Pagination from './Pagination'
+import Note from './Note'
+import { useNote } from './NoteContext'
+
+
+
+const Notes = () => {
+    let notePerPage = 6;
+    const { note } = useNote();
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalPages = Math.ceil(note.length / notePerPage);
+
+
+    const onPageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    return (
+        <div>
+            <Note
+                notes={note}
+                notePerPage={notePerPage}
+                currentPage={currentPage}
+            />
+            {totalPages > 1 ? <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={onPageChange}
+            /> : null}
+        </div>
+    )
+}
+
+export default Notes
